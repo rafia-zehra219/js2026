de578b93368cbdbb2a2a05aa9d2ef720b3b5d28c
rafiazehra219 <rafianadeem438@gmail.com>
2026-04-18 21:26:47 +0500

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+// Question1//
+alert("Eoror!Please enter a valid password");
+
+// Question2 //
+alert("Welcome to JS Land\nHappy Coding!");
+//   Question3 //
+alert("welcome to js land");
+
+// Question4//
+alert("Happy Coding\n\nprevent this page from creating additonal dialogs");
+
+// Question5//
+alert("Hello... i can run js through my web browser console")
