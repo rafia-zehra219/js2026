39d2b425104109a84208b2ae5ba88867264ef654
rafiazehra219 <rafianadeem438@gmail.com>
2026-04-21 21:58:41 +0500

    3rd done

---
+++ assigment3/app.js
@@ -0,0 +1,23 @@
+// Q1:
+var age = 21;
+
+alert("I am " + age + " years old");
+//  Q2:
+var vissited = 14;
+
+alert("you Have " + vissited + "This Site Times ")
+
+// Q3:
+let birthYear = 2004;
+let type = typeof birthYear;
+
+document.write("My birth year is " + birthYear);
+document.write("<br/>");
+document.write("Data type of my declared variable is " + type);
+
+// Q4:
+// let vissitorName = "Ates"
+// let producetTitle = "shirt"
+// let quantity= 10;
+
+// document.write(vissitorName + "orderd"+quantity + "" + producetTitle + "on xyz clothing store")
