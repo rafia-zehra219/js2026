4e053f7d09f599c696242645447d92ce50f4057f
rafiazehra219 <rafianadeem438@gmail.com>
2026-04-21 18:57:10 +0500

    2nd done

---
+++ assigment2/app.js
@@ -0,0 +1,25 @@
+
+// Q1: 
+var userName;
+
+// Q2: 
+var myName = "rafia zehra";
+
+// Q3: 
+var message = "Hello World";
+
+// Q4: 
+alert(message);
+
+// Q5: 
+alert("pizza\pizz\piz\pi\p");
+
+// Q6: 
+var email = "example@gmail.com";
+alert("My email address is " + email);
+
+// Q7: 
+alert("i am trying to learn from the book A smater way to learn js");
+
+// Q8: 
+document.write("Yah! I can write HTML content through JavaScript");
